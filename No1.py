cad0b2f43bfb15673d35924d96205467e7198441
haifaamalinawahdan <haifanew3@gmail.com>
2026-09-23 15:47:36 +0700

    membuat percabangan ketika kondisi grade lebih dari 70 dan 60

--- No1.py
+++ No1.py
@@ -4,3 +4,8 @@ if grade >= 90:
     print("Excellent Performance")
 elif grade >= 80:
     print("Very Good Performance")
+elif grade >= 70:
+    print("Good Performance")
+elif grade >= 60:
+    print("Average Performance")
+    
