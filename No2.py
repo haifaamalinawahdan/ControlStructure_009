b8a63b8f8c79fe583ddf76844e2967e5266b44c7
haifaamalinawahdan <haifanew3@gmail.com>
2026-09-23 15:53:10 +0700

    Membuat percabangan untuk kondisi ketiga variable dengan menggunakan operator logika AND

--- No2.py
+++ No2.py
@@ -1,3 +1,10 @@
 angka1=int(input("Masukkan angka ke-1: "))
 angka2=int(input("Masukkan angka ke-2: ")) 
 angka3=int(input("Masukkan angka ke-3: "))
+
+if angka1>angka2 and angka1>angka3:
+    print("Angka pertama adalah yang terbesar")
+elif angka2>angka1 and angka2>angka3:
+    print("Angka kedua adalah yang terbesar")
+elif angka3>angka1 and angka3>angka2:
+    print("angka ketiga adalah yang terbesar")
